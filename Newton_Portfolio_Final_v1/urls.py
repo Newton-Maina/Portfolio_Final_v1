46a004791256de4e07ae4d3a9732cdeba3e72d68
Newton Maina <newtonmainag@gmail.com>
2024-07-01 12:06:06 +0300

    implement static build file

--- Newton_Portfolio_Final_v1/urls.py
+++ Newton_Portfolio_Final_v1/urls.py
@@ -23,3 +23,5 @@ urlpatterns = [
     path('', include('The_Website_v1.urls'))
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
