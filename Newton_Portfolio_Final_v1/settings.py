f9229b54e12a07150f358f78ef091bd5b36db222
Newton Maina <newtonmainag@gmail.com>
2024-07-01 12:30:33 +0300

    virtualevn configuration

--- Newton_Portfolio_Final_v1/settings.py
+++ Newton_Portfolio_Final_v1/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'The_Website_v1', 'static'),
 
-MEDIA_URL = '/media'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
