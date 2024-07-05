871c1a6f50d3d182514216c30310936d43a27cad
Newton Maina <newtonmainag@gmail.com>
2024-07-05 22:49:57 +0300

    implement contact form functionality

--- Newton_Portfolio_Final_v1/settings.py
+++ Newton_Portfolio_Final_v1/settings.py
@@ -123,10 +123,18 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Email Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT ='587'
+EMAIL_HOST_USER = 'newtonmainag@gmail.com'
+EMAIL_HOST_PASSWORD = 'isru qwme vgyq utbt'
+EMAIL_USE_TLS = True
--- The_Website_v1/views.py
+++ The_Website_v1/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Service
+from django.core.mail import send_mail
 
 # Create your views here.
 def index(request):
@@ -16,4 +17,29 @@ def portfolio(request):
     return render(request, 'portfolio.html',{'navbar': 'portfolio'})
 
 def contact(request):
-    return render(request, 'contact.html',{'navbar': 'contact'})
+    if request.method == "POST":
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        subject = request.POST.get('subject', '')
+        message = request.POST.get('message', '')
+
+        if name and email and subject and message:
+            # Send email
+            send_mail(
+                f'Message from {name}',
+                f'Senders Email: {email} '
+                f' -- Details on message: {message}',
+                email,
+                ['newtonmainag@gmail.com','mainanewtong@gmail.com'],
+            )
+
+            # Display success message in the same template
+            return render(request, 'contact.html', {'success_message': 'Your message has been sent successfully.', 'name': name})
+
+        else:
+            # Handle form validation errors
+            error_message = "Please fill out all required fields."
+            return render(request, 'contact.html', {'error_message': error_message, 'navbar': 'contact'})
+
+    else:
+        return render(request, 'contact.html', {'navbar': 'contact'})
